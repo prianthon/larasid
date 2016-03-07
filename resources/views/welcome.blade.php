a39e1d64654b8d4fa9fcf694666080f1d11da956
Pri Anton Subardio <prianthonsubardio@gmail.com>
2016-03-07 09:34:01 +0700

    desain form pelayanan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,42 +8,47 @@
                 <div class="panel-heading">Selamat datang</div>
 
                 <div class="panel-body">
-                    <div class="col-md-6">
+                    <div class="col-md-4">
                       <button type="button" class="btn btn-success btn-lg btn-block">Semua Pelayanan 1 hari jadi</button>
                       <button type="button" class="btn btn-success btn-lg btn-block">Hanya Dilayani di Kantor Desa</button>
-                      <button type="button" class="btn btn-success btn-lg btn-block">Administrasi Kependudukan GRATIS</button>
+                      <button type="button" class="btn btn-success btn-lg btn-block">Administrasi GRATIS</button>
                       <button type="button" class="btn btn-success btn-lg btn-block">Warga Harus Membawa Syarat</button>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-8">
                       <!--kolom kanan-->
                       <div class="panel panel-primary">
-                        <div class="panel-heading">Pelayanan Publik Desa</div>
+                        <div class="panel-heading"><strong>Pelayanan Publik Desa</strong></div>
                         <div class="panel-body">
                       <form class="form-horizontal">
 					                    <div class="form-group">
-						                    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-						                        <div class="col-sm-10">
-						                            <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+						                    <label for="inputEmail3" class="col-sm-4 control-label">NIK</label>
+						                        <div class="col-sm-8">
+						                            <input type="text" class="form-control" id="inputEmail3" placeholder="">
 						                        </div>
 					                    </div>
 					                    <div class="form-group">
-						                    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-						                        <div class="col-sm-10">
-						                            <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+						                    <label for="inputPassword3" class="col-sm-4 control-label">Nama Lengkap</label>
+						                        <div class="col-sm-8">
+						                            <input type="text" class="form-control" id="inputPassword3" placeholder="">
+						                        </div>
+					                    </div>
+                              <div class="form-group">
+						                    <label for="inputPassword3" class="col-sm-4 control-label">Kebutuhan Pelayanan</label>
+						                        <div class="col-sm-8">
+						                            <select class="form-control">
+                                          <option>-- Klik untuk memilih --</option>
+                                          <option>Surat Permohonan KTP F-1.21</option>
+                                          <option>Surat Pengantar/Keterangan</option>
+                                          <option>Surat Keterangan Bepergian</option>
+                                          <option>Surat Keterangan Domisili</option>
+                                          <option>Surat Keterangan Tidak Mampu</option>
+                                          <option>Surat Pengantar SKCK</option>
+                                        </select>
 						                        </div>
 					                    </div>
 					                    <div class="form-group">
-						                    <div class="col-sm-offset-2 col-sm-10">
-						                        <div class="checkbox">
-							                          <label>
-							                            <input type="checkbox"> Remember me
-							                          </label>
-						                        </div>
-						                    </div>
-					                    </div>
-					                    <div class="form-group">
-						                    <div class="col-sm-offset-2 col-sm-10">
-						                        <button type="submit" class="btn btn-default">Sign in</button>
+						                    <div class="col-sm-offset-4 col-sm-10">
+						                        <button type="submit" class="btn btn-default">KIRIM</button>
 						                    </div>
 					                    </div>
 					            </form>
