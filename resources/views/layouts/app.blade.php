1006ac71bb624bc7f3d7c72d03f2e411b5828ad9
Pri Anton Subardio <prianthonsubardio@gmail.com>
2016-03-07 09:05:07 +0700

    frontpage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Desa2.0</title>
 
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
@@ -40,22 +40,22 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    Desa2.0
                 </a>
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
+                    <li><a href="{{ url('/home') }}">Beranda</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        <li><a href="{{ url('/login') }}">Masuk</a></li>
+                        <!--<li><a href="{{ url('/register') }}">Register</a></li>-->
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -63,7 +63,7 @@
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Keluar</a></li>
                             </ul>
                         </li>
                     @endif
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,52 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Selamat datang</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <div class="col-md-6">
+                      <button type="button" class="btn btn-success btn-lg btn-block">Semua Pelayanan 1 hari jadi</button>
+                      <button type="button" class="btn btn-success btn-lg btn-block">Hanya Dilayani di Kantor Desa</button>
+                      <button type="button" class="btn btn-success btn-lg btn-block">Administrasi Kependudukan GRATIS</button>
+                      <button type="button" class="btn btn-success btn-lg btn-block">Warga Harus Membawa Syarat</button>
+                    </div>
+                    <div class="col-md-6">
+                      <!--kolom kanan-->
+                      <div class="panel panel-primary">
+                        <div class="panel-heading">Pelayanan Publik Desa</div>
+                        <div class="panel-body">
+                      <form class="form-horizontal">
+					                    <div class="form-group">
+						                    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+						                        <div class="col-sm-10">
+						                            <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+						                        </div>
+					                    </div>
+					                    <div class="form-group">
+						                    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+						                        <div class="col-sm-10">
+						                            <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+						                        </div>
+					                    </div>
+					                    <div class="form-group">
+						                    <div class="col-sm-offset-2 col-sm-10">
+						                        <div class="checkbox">
+							                          <label>
+							                            <input type="checkbox"> Remember me
+							                          </label>
+						                        </div>
+						                    </div>
+					                    </div>
+					                    <div class="form-group">
+						                    <div class="col-sm-offset-2 col-sm-10">
+						                        <button type="submit" class="btn btn-default">Sign in</button>
+						                    </div>
+					                    </div>
+					            </form>
+                        </div>
+                      </div>
+                      <!--kolom kanan-->
+                    </div>
                 </div>
             </div>
         </div>
